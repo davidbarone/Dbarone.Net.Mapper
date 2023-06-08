ad02c83faecbebec8c360414d359e26d2d6b7618
davidbarone <davidbarone@live.com>
2023-06-08 23:10:55 +1000

    wip

--- Dbarone.Net.Mapper/Mapper/MapperBuilder.cs
+++ Dbarone.Net.Mapper/Mapper/MapperBuilder.cs
@@ -1,16 +1,20 @@
+namespace Dbarone.Net.Mapper;
+using System.Linq.Expressions;
 
 /// <summary>
 /// Builds a mapping rule.
 /// </summary>
 /// <typeparam name="T"></typeparam>
 /// <typeparam name="U"></typeparam>
-public class MapperBuilder<T, U> {
+public class MapperBuilder<T, U>
+{
 
     /// <summary>
     /// Creates a new MapperBuilder class.
     /// </summary>
     /// <returns></returns>
-    public MapperBuilder<T,U> Create() {
+    public MapperBuilder<T, U> Create()
+    {
         return new MapperBuilder<T, U>();
     }
 
@@ -19,15 +23,59 @@ public class MapperBuilder<T, U> {
     /// </summary>
     /// <param name="convention"></param>
     /// <returns></returns>
-    public MapperBuilder<T,U> SetSourceMemberNamingConvention(NamingConvention convention) {
+    public MapperBuilder<T, U> SetSourceMemberNamingConvention(NamingConvention convention)
+    {
         this.Configuration.SourceMemberNamingConvention = convention;
         return this;
     }
 
-    public MapperBuilder<T, U> SetDestinationMemberNamingConvention(NamingConvention convention) {
+    public MapperBuilder<T, U> SetDestinationMemberNamingConvention(NamingConvention convention)
+    {
         this.Configuration.DestinationMemberNamingConvention = convention;
         return this;
     }
 
+    /// <summary>
+    /// Define which property will not be mapped to document
+    /// </summary>
+    public MapperBuilder<T, U> Ignore<K>(Expression<Func<T, K>> member)
+    {
+        return this.ApplyMemberAction(member, (p) =>
+        {
+            Configuration.Members.Remove(p);
+        });
+    }
+
+    /// <summary>
+    /// Define a custom name for a property when mapping to document
+    /// </summary>
+    public MapperBuilder<T, U> Rename<K>(Expression<Func<T, K>> member, string newName)
+    {
+        if (newName.IsNullOrWhiteSpace()) throw new ArgumentNullException(nameof(newName));
+
+        return this.ApplyMemberAction(member, (p) =>
+        {
+            p.FieldName = newName;
+        });
+    }
+
+    /// <summary>
+    /// Get a property based on a expression. Eg.: 'x => x.UserId' return string "UserId"
+    /// </summary>
+    private MapperBuilder<T, U> ApplyMemberAction<TK, K>(Expression<Func<TK, K>> member, Action<MemberRule> action)
+    {
+        if (member == null) throw new ArgumentNullException(nameof(member));
+
+        var memb = Configuration.GetMemberRule(member);
+
+        if (memb == null)
+        {
+            throw new ArgumentNullException($"Member '{member.GetPath()}' not found in type '{_entity.ForType.Name}' (use IncludeFields in BsonMapper)");
+        }
+
+        action(memb);
+
+        return this;
+    }
     public EntityMap Configuration { get; } = new EntityMap();
 }
--- Dbarone.Net.Mapper/Mapper/MemberRule.cs
+++ Dbarone.Net.Mapper/Mapper/MemberRule.cs
@@ -1,31 +1,32 @@
+namespace Dbarone.Net.Mapper;
+
 using System;
 using System.Reflection;
 
-namespace Dbarone.Net.Mapper
+/// <summary>
+/// Defines the rules for a single member mapping.
+/// </summary>
+public class MemberRule
 {
     /// <summary>
-    /// Defines the rules for a single member mapping.
+    /// Member name for the mapping rule.
     /// </summary>
-    public class MemberRule
-    {
-        /// <summary>
-        /// Member name for the mapping rule.
-        /// </summary>
-        public string MemberName { get; set; } = default!;
+    public string MemberName { get; set; } = default!;
 
-        /// <summary>
-        /// Member data type.
-        /// </summary>
-        public Type DataType { get; set; } = default!;
+    /// <summary>
+    /// Member data type.
+    /// </summary>
+    public Type DataType { get; set; } = default!;
 
-        /// <summary>
-        /// Delegate method to get the value from the instance.
-        /// </summary>
-        public Getter Getter { get; set; } = default!;
+    public string MemberOverrideName { get; set; } = default!;
 
-        /// <summary>
-        /// Delegate method to set the value to the instance.
-        /// </summary>
-        public Setter Setter { get; set; } = default!;
-    }
+    /// <summary>
+    /// Delegate method to get the value from the instance.
+    /// </summary>
+    public Getter Getter { get; set; } = default!;
+
+    /// <summary>
+    /// Delegate method to set the value to the instance.
+    /// </summary>
+    public Setter Setter { get; set; } = default!;
 }
