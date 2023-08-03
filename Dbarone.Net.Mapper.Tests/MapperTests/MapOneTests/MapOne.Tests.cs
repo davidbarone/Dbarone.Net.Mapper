638ffa0fd432bb05c385b4b9abd2391d36d0bd49
davidbarone <davidbarone@live.com>
2023-08-03 12:23:09 +1000

    Testing

--- Dbarone.Net.Mapper.Tests/MapperTests/MapOneTests/MapOne.Tests.cs
+++ Dbarone.Net.Mapper.Tests/MapperTests/MapOneTests/MapOne.Tests.cs
@@ -31,9 +31,26 @@ public class MapOneTests
 
    [Fact]
     public void Should_Map_Enum_Types() {
-        EnumType obj1 = EnumType.CreateMin();
+        EnumType obj1 = EnumType.CreateMax();
         var mapper = MapperConfiguration.Create().RegisterType<EnumType>().Build();
         var obj2 = mapper.MapOne<EnumType, EnumType>(obj1);
         Assert.True(obj1.ValueEquals(obj2));
     }
+
+   [Fact]
+    public void Should_Map_Enum_Types_Nullable() {
+        EnumTypeNullable obj1 = EnumTypeNullable.CreateNull();
+        var mapper = MapperConfiguration.Create().RegisterType<EnumTypeNullable>().Build();
+        var obj2 = mapper.MapOne<EnumTypeNullable, EnumTypeNullable>(obj1);
+        Assert.True(obj1.ValueEquals(obj2));
+    }
+
+   [Fact]
+    public void Should_Map_Enum_Types_Nullable_Max() {
+        EnumTypeNullable obj1 = EnumTypeNullable.CreateMax();
+        var mapper = MapperConfiguration.Create().RegisterType<EnumTypeNullable>().Build();
+        var obj2 = mapper.MapOne<EnumTypeNullable, EnumTypeNullable>(obj1);
+        Assert.True(obj1.ValueEquals(obj2));
+    }
+
 }
