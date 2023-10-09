5f8307ffc186e33aebff47701e1e23865ba2c27e
davidbarone <davidbarone@live.com>
2023-10-09 19:51:12 +1100

    refactoring

--- Dbarone.Net.Mapper/Mapper/MapperBuilder.cs
+++ Dbarone.Net.Mapper/Mapper/MapperBuilder.cs
@@ -1,28 +1,127 @@
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Dbarone.Net.Mapper;
 
 /// <summary>
 /// Takes a <see cref="MapperConfiguration" /> instance, and builds a mapper to handle the mapping between 2 types.
 /// </summary>
-public class MapperBuilder {
+public class MapperBuilder
+{
     private Config Configuration { get; set; }
-    public MapperBuilder(Config configuration) {
+    public MapperBuilder(Config configuration)
+    {
         this.Configuration = configuration;
     }
 
-    public ObjectMapper GetMapper<TSource, TDestination>(){
+    /// <summary>
+    /// Returns true if the source and destination mapper combination has been previously
+    /// built and saved to the cache.
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TDestination"></typeparam>
+    /// <returns></returns>
+    public bool HasMapper<TSource, TDestination>()
+    {
+
+    }
+
+    /// <summary>
+    /// Gets a mapper for a source and destination type combination. If the source and destination
+    /// mapper has not been retrieved before, a one-time build process occurs to generate all the
+    /// mapping rules, and a validation also takes place.
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TDestination"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="NotSupportedException"></exception>
+    public ObjectMapper GetMapper<TSource, TDestination>()
+    {
         throw new NotSupportedException();
     }
-    public GetMapper(Type fromType, Type toType) {
+    public GetMapper(Type fromType, Type toType)
+    {
 
+    }
+
+    /// <summary>
+    /// Validates the source and destinations in respect of the end-point validation rules provided. 
+    /// </summary>
+    /// <returns></returns>
+    private List<MapperBuildNotification> EndPointValidation(MapperTypeConfiguration sourceConfiguration, MapperTypeConfiguration destinationConfiguration)
+    {
+
+        List<MapperBuildNotification> notifications = new List<MapperBuildNotification>();
+
+        if ((sourceConfiguration.Options.EndPointValidation & MapperEndPoint.Source) == MapperEndPoint.Source)
+        {
+            // check all source member rules map to destination rules.
+            var unmappedSourceMembers = sourceConfiguration
+                .MemberConfiguration
+                .Where(m => destinationConfiguration
+                    .MemberConfiguration
+                    .Select(d => d.InternalMemberName).Contains(m.InternalMemberName) == false);
+
+            foreach (var item in unmappedSourceMembers)
+            {
+                notifications.Add(new MapperBuildNotification
+                {
+                    SourceType = sourceConfiguration.Type,
+                    DestinationType = destinationConfiguration.Type,
+                    MemberName = item.MemberName,
+                    Message = "Source end point validation enabled, but source member is not mapped to destination."
+                });
+            }
+        }
+
+        if ((destinationConfiguration.Options.EndPointValidation & MapperEndPoint.Destination) == MapperEndPoint.Destination)
+        {
+            // check all source member rules map to destination rules.
+            var unmappedDestinationMembers = destinationConfiguration
+                .MemberConfiguration
+                .Where(m => sourceConfiguration
+                    .MemberConfiguration
+                    .Select(d => d.InternalMemberName).Contains(m.InternalMemberName) == false);
+
+            foreach (var item in unmappedDestinationMembers)
+            {
+                notifications.Add(new MapperBuildNotification
+                {
+                    SourceType = sourceConfiguration.Type,
+                    DestinationType = destinationConfiguration.Type,
+                    MemberName = item.MemberName,
+                    Message = "Destination end point validation enabled, but destination member is not mapped from source."
+                });
+            }
+        }
+
+        return notifications;
     }
 
 
     private IEnumerable<MapperDelegate> Build(Type sourceType, Type destinationType)
     {
+        List<MapperBuildNotification> notifications = new List<MapperBuildNotification>();
+        var sourceConfig = this.Configuration.Types.FirstOrDefault(c => c.Value.Type == sourceType).Value;
+        var destinationConfig = this.Configuration.Types.FirstOrDefault(c => c.Value.Type == destinationType).Value;
+
+        if (destinationType.IsInterface)
+        {
+            throw new MapperException($"Destination type cannot be interface type.");
+        }
+
         // Are both source and destination types registered?
-        if (Configuration.)
+        if (sourceConfig != null && destinationConfig !=null)
+        {
+            // both source and destination types registered - normal mapping process
+
+            notifications.AddRange(EndPointValidation(sourceConfig, destinationConfig));
+            // do end point validation here.
+
+
+
+
+        }
 
         // validation
         if (!configuration.ContainsKey(sourceType))
@@ -35,43 +134,8 @@ public class MapperBuilder {
             throw new MapperException($"Destination type: [{destinationType.Name}] not registered.");
         }
 
-        if (destinationType.IsInterface)
-        {
-            throw new MapperException($"Destination type cannot be interface type.");
-        }
 
-        var sourceConfig = this.configuration.First(c => c.Value.Type == sourceType).Value;
-        var destinationConfig = this.configuration.First(c => c.Value.Type == destinationType).Value;
 
-        if ((sourceConfig.Options.EndPointValidation & MapperEndPoint.Source) == MapperEndPoint.Source)
-        {
-            // check all source member rules map to destination rules.
-            var unmappedSourceMembers = sourceConfig
-                .MemberConfiguration
-                .Where(m => destinationConfig
-                    .MemberConfiguration
-                    .Select(d => d.InternalMemberName).Contains(m.InternalMemberName) == false);
-            if (unmappedSourceMembers.Count() > 0)
-            {
-                var errorMembers = unmappedSourceMembers.Select(m => m.MemberName).Aggregate("", (current, next) => current + " " + $"[{next}]");
-                throw new MapperException($"The following source members are not mapped: {errorMembers}.");
-            }
-        }
-
-        if ((destinationConfig.Options.EndPointValidation & MapperEndPoint.Destination) == MapperEndPoint.Destination)
-        {
-            // check all source member rules map to destination rules.
-            var unmappedDestinationMembers = destinationConfig
-                .MemberConfiguration
-                .Where(m => sourceConfig
-                    .MemberConfiguration
-                    .Select(d => d.InternalMemberName).Contains(m.InternalMemberName) == false);
-            if (unmappedDestinationMembers.Count() > 0)
-            {
-                var errorMembers = unmappedDestinationMembers.Select(m => m.MemberName).Aggregate("", (current, next) => current + " " + $"[{next}]");
-                throw new MapperException($"The following destination members are not mapped: {errorMembers}.");
-            }
-        }
 
         // Get internal member names matching on source + destination
         IEnumerable<string> matchedMembers = destinationConfig
