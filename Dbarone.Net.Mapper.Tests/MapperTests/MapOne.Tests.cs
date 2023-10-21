23c1664e13ce2a7650eb7b73da73fe320f50c3ad
davidbarone <davidbarone@live.com>
2023-10-22 09:34:40 +1100

    in progress

--- Dbarone.Net.Mapper.Tests/MapperTests/MapOne.Tests.cs
+++ Dbarone.Net.Mapper.Tests/MapperTests/MapOne.Tests.cs
@@ -5,30 +5,6 @@ using System.Linq.Expressions;
 
 public class MapOneTests
 {
-    [Fact]
-    public void Should_Map_Builtin_Value_Types_Min() {
-        BuiltinValueTypes obj1 = BuiltinValueTypes.CreateMin();
-        var mapper = MapperConfiguration.Create().RegisterType<BuiltinValueTypes>().Build();
-        var obj2 = mapper.MapOne<BuiltinValueTypes, BuiltinValueTypes>(obj1);
-        Assert.True(obj1.ValueEquals(obj2));
-    }
-
-    [Fact]
-    public void Should_Map_Builtin_Value_Types_Max() {
-        BuiltinValueTypes obj1 = BuiltinValueTypes.CreateMax();
-        var mapper = MapperConfiguration.Create().RegisterType<BuiltinValueTypes>().Build();
-        var obj2 = mapper.MapOne<BuiltinValueTypes, BuiltinValueTypes>(obj1);
-        Assert.True(obj1.ValueEquals(obj2));
-    }
-
-    [Fact]
-    public void Should_Map_Builtin_Value_Types_Nullable() {
-        BuiltinValueTypesNullable obj1 = BuiltinValueTypesNullable.CreateNull();
-        var mapper = MapperConfiguration.Create().RegisterType<BuiltinValueTypesNullable>().Build();
-        var obj2 = mapper.MapOne<BuiltinValueTypesNullable, BuiltinValueTypesNullable>(obj1);
-        Assert.True(obj1.ValueEquals(obj2));
-    }
-
    [Fact]
     public void Should_Map_Enum_Types() {
         EnumType obj1 = EnumType.CreateMax();
