9bb8be758c387283ac542733d46ae03a131ec8c1
davidbarone <davidbarone@live.com>
2023-10-22 10:18:50 +1100

    refactoring complete. building again.

--- Dbarone.Net.Mapper.Tests/MapperTests/MapDictionary.Tests.cs
+++ Dbarone.Net.Mapper.Tests/MapperTests/MapDictionary.Tests.cs
@@ -4,6 +4,11 @@ using Dbarone.Net.Extensions.Object;
 using System.Linq.Expressions;
 using System.Collections;
 
+public class DictTestClass
+{
+    public int IntValue { get; set; } = int.MaxValue;
+}
+
 public class MapDictionaryTests
 {
     
@@ -15,28 +20,21 @@ public class MapDictionaryTests
         dict1["MemberB"] = int.MaxValue;
         dict1["MemberC"] = "foobar";
 
-        var mapper = MapperConfiguration
-        .Create()
-        .RegisterType<Dictionary<string, object>>()
-        .Build();
+        var mapper = new ObjectMapper(new MapperConfiguration()
+            .RegisterType<Dictionary<string, object>>());
 
         var dict2 = mapper.MapOne<Dictionary<string, object>, Dictionary<string, object>>(dict1);
-
         Assert.True(dict2.ValueEquals(dict1));
     }
 
     [Fact]
     public void TestClassToDict() {
         ClassA a = new ClassA();
-
-        var mapper = MapperConfiguration
-        .Create()
-        .RegisterType<Dictionary<string, object>>()
-        .RegisterType<ClassA>()
-        .Build();
+        var mapper = new ObjectMapper(new MapperConfiguration()
+            .RegisterType<Dictionary<string, object>>()
+            .RegisterType<DictTestClass>());
 
         var dict = mapper.MapOne<ClassA, Dictionary<string, object>>(a);
-
         Assert.Equal(a.IntValue, dict["IntValue"]);
     }
 }
