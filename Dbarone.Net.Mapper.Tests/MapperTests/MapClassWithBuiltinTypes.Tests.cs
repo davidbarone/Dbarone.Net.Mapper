d024bf7c85d628895c6d07be6d8b622030dbaa19
davidbarone <davidbarone@live.com>
2023-10-22 09:32:25 +1100

    in progress

---
+++ Dbarone.Net.Mapper.Tests/MapperTests/MapClassWithBuiltinTypes.Tests.cs
@@ -0,0 +1,184 @@
+namespace Dbarone.Net.Mapper.Tests;
+using Dbarone.Net.Mapper;
+using Dbarone.Net.Extensions.Object;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+
+public class BuiltinValueTypes
+{
+    public bool BoolValue { get; set; }
+    public byte ByteValue { get; set; }
+    public sbyte SByteValue { get; set; }
+    public char CharValue { get; set; }
+    public decimal DecimalValue { get; set; }
+    public double DoubleValue { get; set; }
+    public float FloatValue { get; set; }
+    public int IntValue { get; set; }
+    public uint UIntValue { get; set; }
+    public nint NIntValue { get; set; }
+    public nuint NUIntValue { get; set; }
+    public long LongValue { get; set; }
+    public ulong ULongValue { get; set; }
+    public short ShortValue { get; set; }
+    public ushort UShortValue { get; set; }
+
+    public static BuiltinValueTypes CreateMin()
+    {
+        return new BuiltinValueTypes
+        {
+            BoolValue = false,
+            ByteValue = byte.MinValue,
+            SByteValue = sbyte.MinValue,
+            CharValue = char.MinValue,
+            DecimalValue = decimal.MinValue,
+            DoubleValue = double.MinValue,
+            FloatValue = float.MinValue,
+            IntValue = int.MinValue,
+            UIntValue = uint.MinValue,
+            NIntValue = nint.MinValue,
+            NUIntValue = nuint.MinValue,
+            LongValue = long.MinValue,
+            ULongValue = ulong.MinValue,
+            ShortValue = short.MinValue,
+            UShortValue = ushort.MinValue
+        };
+    }
+    public static BuiltinValueTypes CreateMax()
+    {
+        return new BuiltinValueTypes
+        {
+            BoolValue = true,
+            ByteValue = byte.MaxValue,
+            SByteValue = sbyte.MaxValue,
+            CharValue = char.MaxValue,
+            DecimalValue = decimal.MaxValue,
+            DoubleValue = double.MaxValue,
+            FloatValue = float.MaxValue,
+            IntValue = int.MaxValue,
+            UIntValue = uint.MaxValue,
+            NIntValue = nint.MaxValue,
+            NUIntValue = nuint.MaxValue,
+            LongValue = long.MaxValue,
+            ULongValue = ulong.MaxValue,
+            ShortValue = short.MaxValue,
+            UShortValue = ushort.MaxValue
+        };
+    }
+}
+
+public class BuiltinValueTypesNullable {
+    public bool? BoolValue { get; set; }
+    public byte? ByteValue { get; set; }
+    public sbyte? SByteValue { get; set; }
+    public char? CharValue { get; set; }
+    public decimal? DecimalValue { get; set; }
+    public double? DoubleValue { get; set; }
+    public float? FloatValue { get; set; }
+    public int? IntValue { get; set; }
+    public uint? UIntValue { get; set; }
+    public nint? NIntValue { get; set; }
+    public nuint? NUIntValue { get; set; }
+    public long? LongValue { get; set; }
+    public ulong? ULongValue { get; set; }
+    public short? ShortValue { get; set; }
+    public ushort? UShortValue { get; set; }
+
+    public static BuiltinValueTypesNullable CreateMin() {
+        return new BuiltinValueTypesNullable
+        {
+            BoolValue = false,
+            ByteValue = byte.MinValue,
+            SByteValue = sbyte.MinValue,
+            CharValue = char.MinValue,
+            DecimalValue = decimal.MinValue,
+            DoubleValue = double.MinValue,
+            FloatValue = float.MinValue,
+            IntValue = int.MinValue,
+            UIntValue = uint.MinValue,
+            NIntValue = nint.MinValue,
+            NUIntValue = nuint.MinValue,
+            LongValue = long.MinValue,
+            ULongValue = ulong.MinValue,
+            ShortValue = short.MinValue,
+            UShortValue = ushort.MinValue
+        };
+    }
+ 
+    public static BuiltinValueTypesNullable CreateMax() {
+        return new BuiltinValueTypesNullable
+        {
+            BoolValue = true,
+            ByteValue = byte.MaxValue,
+            SByteValue = sbyte.MaxValue,
+            CharValue = char.MaxValue,
+            DecimalValue = decimal.MaxValue,
+            DoubleValue = double.MaxValue,
+            FloatValue = float.MaxValue,
+            IntValue = int.MaxValue,
+            UIntValue = uint.MaxValue,
+            NIntValue = nint.MaxValue,
+            NUIntValue = nuint.MaxValue,
+            LongValue = long.MaxValue,
+            ULongValue = ulong.MaxValue,
+            ShortValue = short.MaxValue,
+            UShortValue = ushort.MaxValue
+        };
+    }
+
+   public static BuiltinValueTypesNullable CreateNull() {
+        return new BuiltinValueTypesNullable
+        {
+            BoolValue = null,
+            ByteValue = null,
+            SByteValue = null,
+            CharValue = null,
+            DecimalValue = null,
+            DoubleValue = null,
+            FloatValue = null,
+            IntValue = null,
+            UIntValue = null,
+            NIntValue = null,
+            NUIntValue = null,
+            LongValue = null,
+            ULongValue = null,
+            ShortValue = null,
+            UShortValue = null
+        };
+    }
+}
+
+public class MapClassWithBuiltinTypesTests
+{
+    [Fact]
+    public void Should_Map_Builtin_Value_Types_Min()
+    {
+        BuiltinValueTypes obj1 = BuiltinValueTypes.CreateMin();
+        var mapper = new ObjectMapper(new MapperConfiguration()
+            .RegisterType<BuiltinValueTypes>()
+        );
+        var obj2 = mapper.MapOne<BuiltinValueTypes, BuiltinValueTypes>(obj1);
+        Assert.True(obj1.ValueEquals(obj2));
+    }
+
+    [Fact]
+    public void Should_Map_Builtin_Value_Types_Max()
+    {
+        BuiltinValueTypes obj1 = BuiltinValueTypes.CreateMin();
+        var mapper = new ObjectMapper(new MapperConfiguration()
+            .RegisterType<BuiltinValueTypes>()
+        );
+        var obj2 = mapper.MapOne<BuiltinValueTypes, BuiltinValueTypes>(obj1);
+        Assert.True(obj1.ValueEquals(obj2));
+    }
+
+    [Fact]
+    public void Should_Map_Builtin_Value_Types_Nullable()
+    {
+        BuiltinValueTypesNullable obj1 = BuiltinValueTypesNullable.CreateNull();
+        var mapper = new ObjectMapper(new MapperConfiguration()
+            .RegisterType<BuiltinValueTypesNullable>()
+        );
+        var obj2 = mapper.MapOne<BuiltinValueTypesNullable, BuiltinValueTypesNullable>(obj1);
+        Assert.True(obj1.ValueEquals(obj2));
+    }
+}
