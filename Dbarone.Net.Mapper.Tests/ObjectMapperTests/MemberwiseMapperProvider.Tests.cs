601c3acc3268bf7b6fce1bfdf8a0371b54aedbb8
davidbarone <davidbarone@live.com>
2023-11-11 16:59:11 +1100

    Added tests

--- Dbarone.Net.Mapper.Tests/ObjectMapperTests/MemberwiseMapperProvider.Tests.cs
+++ Dbarone.Net.Mapper.Tests/ObjectMapperTests/MemberwiseMapperProvider.Tests.cs
@@ -3,23 +3,48 @@ using Dbarone.Net.Mapper;
 public class CustomerEntity {
     public int CustomerId { get; set; }
     public string CustomerName { get; set; }
+    public AddressEntity Address { get; set; }
 }
 
 public class CustomerDto {
     public int CustomerId { get; set; }
     public string CustomerName { get; set; }
+    public AddressDto Address { get; set; }
+}
+
+public class AddressEntity {
+    public int AddressId { get; set; }
+    public string AddressLine1 { get; set; }
+}
+
+public class AddressDto {
+    public int AddressId { get; set; }
+    public string AddressLine1 { get; set; }
 }
 
 public class MemberwiseMapperProviderTests {
 
     [Fact]
-    public void MapCustomer() {
+    public void MemberMapping() {
         var mapper = new ObjectMapper(new MapperConfiguration()
             .SetAutoRegisterTypes(true)
         );
 
-        var customerA = new CustomerEntity() { CustomerId = 123, CustomerName = "Test Customer" }; 
-        var customerB = mapper.Map<CustomerEntity, CustomerDto>(customerA);
-        Assert.Equal("Test Customer", customerB.CustomerName);
+        var addressA = new AddressEntity() { AddressId = 123, AddressLine1 = "Test Address" }; 
+        var addressB = mapper.Map<AddressEntity, AddressDto>(addressA);
+        Assert.Equal("Test Address", addressB.AddressLine1);
     }
+
+    [Fact]
+    public void NestedMemberMapping() {
+        var mapper = new ObjectMapper(new MapperConfiguration()
+            .SetAutoRegisterTypes(true)
+        );
+
+        var addressA = new AddressEntity() { AddressId = 123, AddressLine1 = "Test Address" };
+        var customerA = new CustomerEntity() { CustomerId = 1, CustomerName = "Test Customer", Address = addressA };
+        var customerB = mapper.Map<CustomerEntity, CustomerDto>(customerA);
+        Assert.Equal("Test Address", customerB.Address.AddressLine1);
+    }
+
 }
