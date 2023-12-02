295b4aaec370d55d6b08fc0368912997fad3d9be
davidbarone <davidbarone@live.com>
2023-12-02 15:05:02 +1100

    EnumerableBuffer tests

---
+++ Dbarone.Net.Mapper.Tests/OtherTests/EnumerableBuffer.Tests.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+using Dbarone.Net.Extensions;
+
+public class EnumerableBufferTests
+{
+    [Fact]
+    public void TestToArrayList()
+    {
+        object[] arr = new object[] { 1, 2, 3, 4, 5 };
+        int expectedLength = arr.Length;
+        var buffer = new EnumerableBuffer(arr);
+        var converted = buffer.ToArrayList();
+        Assert.Equal(expectedLength, converted.Count);
+        Assert.IsType<ArrayList>(converted);
+    }
+
+    [Fact]
+    public void TestToQueue()
+    {
+        object[] arr = new object[] { 1, 2, 3, 4, 5 };
+        int expectedLength = arr.Length;
+        var buffer = new EnumerableBuffer(arr);
+        var converted = buffer.ToQueue();
+        Assert.Equal(expectedLength, converted.Count);
+        Assert.IsType<Queue>(converted);
+    }
+
+    [Fact]
+    public void TestToStack()
+    {
+        object[] arr = new object[] { 1, 2, 3, 4, 5 };
+        int expectedLength = arr.Length;
+        var buffer = new EnumerableBuffer(arr);
+        var converted = buffer.ToStack();
+        Assert.Equal(expectedLength, converted.Count);
+        Assert.IsType<Stack>(converted);
+    }
+
+    [Fact]
+    public void TestToGenericList()
+    {
+        object[] arr = new object[] { 1, 2, 3, 4, 5 };
+        int expectedLength = arr.Length;
+        var buffer = new EnumerableBuffer(arr);
+        var converted = buffer.ToGenericList<int>();
+        Assert.Equal(expectedLength, converted.Count);
+        Assert.IsType<List<int>>(converted);
+    }
+
+    [Fact]
+    public void TestToGenericEnumerable()
+    {
+        object[] arr = new object[] { 1, 2, 3, 4, 5 };
+        int expectedLength = arr.Length;
+        var buffer = new EnumerableBuffer(arr);
+        var converted = buffer.ToGenericEnumerable<int>();
+        Assert.Equal(expectedLength, converted.Count());
+        Assert.IsAssignableFrom<IEnumerable<int>>(converted);
+    }
+
+    [Fact]
+    public void TestEnumerableBufferWithMapper()
+    {
+        object[] arr = new object[] { 1, 2, 3, 4, 5 };
+        // Mapper converts int to DateTime
+        MapperDelegate mapper = (s, t) => { return DateTime.Now.AddDays((int)s!); };
+        int expectedLength = arr.Length;
+        var buffer = new EnumerableBuffer(arr, mapper);
+        var converted = buffer.ToArray<DateTime>();
+        Assert.Equal(expectedLength, converted.Count());
+        Assert.IsType<DateTime[]>(converted);
+    }
+}
